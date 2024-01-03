232e04536c13a893a1de19424ccefa7124c6f922
Jhuan <jhuan.ysb@gmail.com>
2024-01-02 22:02:01 -0300

    Mudança 006:
    A animação do botão de filtro foi feita, não certeza se vai ser essa mesmo, mas foi a unica que consegui fazer no momento. Por enqunato apenas isso, estou pensando em começar a criação das outras paginas que vão conter outros livros... Ainda estou pensando nisso.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navegacao = document.querySelector(".barra-navegação")
 
+const filter = document.querySelector(".filter");
+const navCate = document.querySelector(".nav-categorias");
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     navegacao.classList.toggle('active');
 })
+
+filter.addEventListener("click", () => {
+    navCate.classList.toggle('ativar');
+})
+
